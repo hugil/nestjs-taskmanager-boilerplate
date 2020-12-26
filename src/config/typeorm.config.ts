59ba67a0a033d733f88981590ad5c4d0fcf3c410
hugil <hugo.gillberg@outlook.com>
2020-12-26 15:26:39 +0100

    first commit

---
+++ src/config/typeorm.config.ts
@@ -0,0 +1,9 @@
+import { TypeOrmModuleOptions } from "@nestjs/typeorm";
+
+export const typeOrmConfig: TypeOrmModuleOptions = {
+    type: 'sqlite',
+    database: './db.sqlite',
+    entities: [__dirname + '/../**/*.entity{.ts,.js}'],
+    synchronize: true,
+};
+
